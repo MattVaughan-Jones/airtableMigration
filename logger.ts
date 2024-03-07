57c71bbd656a48185443d7a37ccbec944dc003d9
MV-J <mattvj97@gmail.com>
2024-03-07 13:40:50 +1100

    removed redundant loggin files

--- logger.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-const outputLog = fs.createWriteStream('./outputLog.log');
-const errorsLog = fs.createWriteStream('./errorsLog.log');
-
-export const consoler = new console.Console(outputLog, errorsLog);
