b7db079d7dc163bf73de61278d9065bed539d974
MV-J <mattvj97@gmail.com>
2024-03-20 20:20:44 +1100

    add /tmp to .gitignore

--- test.js
+++
@@ -1,3 +0,0 @@
-let buffer = '89 50 4e 47 0d 0a 1a 0a 00 00 00 0d 49 48 44 52 00 00 00 20 00 00 00 20 08 06 00 00 00 73 7a 7a f4 00 00 00 09 70 48 59 73 00 00 0b 13 00 00 0b 13 01'
-let arr = buffer.split(' ');
-console.log(arr.length);
